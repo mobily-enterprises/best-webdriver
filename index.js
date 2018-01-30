821b317e4db2f52931f2064176bb512cd4ce7b5f
Tony Mobily <tonymobily@gmail.com>
2018-01-31 00:09:07 +0800

    Added link to mixins

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const browser = require('./src/browsers/index')
 
 exports = module.exports = { Driver, browser, Actions }
 
+
 // "docs": "rm -rf docs/*;node_modules/jsdoc/jsdoc.js --template node_modules/jsdoc-template -c .jsdoc.config.js -R README.md -P package.json; mv _out/*/*/** docs;rm -rf _out;touch docs/.nojekyll"
 
 /*
@@ -29,6 +30,7 @@ exports = module.exports = { Driver, browser, Actions }
       [X] Make sure Globals are indexed right
       [X] Document Pointer and Keyboard in Actions
     [ ] Write guide in README.md
+    [ ] Make mixins work https://github.com/jsdoc3/jsdoc/issues/1409
   [X] Submit code for review on codereview
   [ ] Write initial tests (ah!)
   [ ] Write more tests
