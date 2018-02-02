58ed0bd4124f9ecc92efd4571d37082f6c81cd5f
Tony Mobily <tonymobily@gmail.com>
2018-02-03 00:49:31 +0800

    We seem to have tests

--- tests/index.js
+++ tests/index.js
@@ -7,10 +7,6 @@ const path = require('path')
 // chai.should()
 const expect = chai.expect
 
-var allBrowsers = [ browser.Chrome, browser.Firefox, browser.Edge, browser.Safari ]
-var browsers = []
-var server
-
 async function browserThere (Browser) {
   var browser = new Browser()
   var driver = new Driver(browser)
@@ -39,31 +35,12 @@ function startServer () {
   })
 }
 
-function itAll (description, cb) {
-  browsers.forEach((Browser) => {
-    var driver = new Driver(new Browser())
-    it(`[${driver._browser.name}] ${description}`, async function () {
-      this.timeout(5000)
-      await driver.newSession()
-      await cb.call(this, driver)
-      await driver.deleteSession()
-      await driver.stopWebDriver()
-    })
-  })
+function sleep (ms) { // eslint-disable-line no-unused-vars
+  return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-function itAllClean (description, cb) {
-  browsers.forEach((Browser) => {
-    var driver = new Driver(new Browser())
-    it(`[${driver._browser.name}] ${description}`, async function () {
-      await cb.call(this, driver)
-    })
-  })
-}
-
-function sleep (ms) { return new Promise(resolve => setTimeout(resolve, ms)) }
-
-(async () => {
+async function getActiveBrowsers (allBrowsers) {
+  var browsers = []
   for (var i = 0, l = allBrowsers.length; i < l; i++) {
     var Browser = allBrowsers[i]
     if (await browserThere(Browser)) {
@@ -73,157 +50,178 @@ function sleep (ms) { return new Promise(resolve => setTimeout(resolve, ms)) }
   if (!browsers.length) {
     throw new Error('No webdrivers found')
   }
+  return browsers
+}
 
-  // Start the local HTTP server
-  server = await startServer()
+(async () => {
+  var browsers = await getActiveBrowsers([ browser.Chrome, browser.Firefox, browser.Edge, browser.Safari ])
+  var server = await startServer()
   var port = server.address().port
   console.log(`Started local server: http://127.0.0.1:${port}/`)
-  console.log('DONE:', browsers)
 
-  /* eslint-disable no-unused-expressions */
-  /* globals describe,it,after,run */
-  describe('start all tests', async function () {
-    this.timeout(2003)
+  browsers.forEach((Browser) => {
+    var browser = new Browser()
+    var browserName = browser.name
 
-    // Close things up
-    after(async function () {
-      server.close()
-    })
-
-    describe('basic non-element calls', async function () {
-      itAllClean('status (no session)', async function (driver) {
-        await driver.startWebDriver()
-        var status = await driver.status()
-        expect(status).to.be.a('object')
-        expect(true).to.be.true
-        await driver.stopWebDriver()
+    /* eslint-disable no-unused-expressions */
+    /* globals describe,it,before,after,run */
+    describe(`[${browserName}] start all tests`, async function () {
+      // Close things up
+      after(async function () {
+        server.close()
       })
 
-      itAll('status', async function (driver) {
-        var status = await driver.status()
-        expect(status).to.be.a('object')
-        expect(true).to.be.true
+      // ***********************************************
+      // ************ BASIC NON-ELEMENT CALLS **********
+      // ***********************************************
+
+      describe(`[${browserName}] basic non-element calls`, async function () {
+        var driver
+
+        // Close things up
+        before(async function () {
+          driver = new Driver(new Browser())
+          await driver.startWebDriver()
+        })
+
+        // Close things up
+        after(async function () {
+          await driver.deleteSession()
+          await driver.stopWebDriver()
+        })
+
+        it('check the status', async function () {
+          var status = await driver.status()
+          expect(status).to.be.an('object')
+          expect(status).to.have.property('ready')
+        })
+
+        it('opens a new session', async function () {
+          this.timeout(5000)
+          var session = await driver.newSession()
+          expect(session).to.be.an('object')
+        })
+
+        it('timeouts', async function () {
+          expect(true).to.be.true
+        })
+        it('navigateTo/getCurrentUrl', async function () {
+          expect(true).to.be.true
+        })
+        it('back/forward/refresh', async function () {
+          expect(true).to.be.true
+        })
+        it('getTitle', async function () {
+          expect(true).to.be.true
+        })
+        it('getWindowHandles/switchToWindow', async function () {
+          expect(true).to.be.true
+        })
+        it('switchToFrame/switchToParentFrame', async function () {
+          expect(true).to.be.true
+        })
+        it('getWindowRect/setWindowRect', async function () {
+          expect(true).to.be.true
+        })
+        it('maximizeWindow/minimizeWindow/fullScreenWindow', async function () {
+          expect(true).to.be.true
+        })
+        it('getPageSource', async function () {
+          expect(true).to.be.true
+        })
+        it('executeScript/executeAsyncScript', async function () {
+          expect(true).to.be.true
+        })
+        it('getAllCookies/getNamedCookie/addCookie/deleteCookie/deleteAllCookies', async function () {
+          expect(true).to.be.true
+        })
+        it('dismissAlert/acceptAlert/getAlertText/sendAlertText', async function () {
+          expect(true).to.be.true
+        })
+        it('takeScreenshot', async function () {
+          expect(true).to.be.true
+        })
+        it('getActiveElement', async function () {
+          expect(true).to.be.true
+        })
       })
 
-      itAll('timeouts', async function () {
-        expect(true).to.be.true
-      })
-      itAll('navigateTo/getCurrentUrl', async function () {
-        expect(true).to.be.true
-      })
-      itAll('back/forward/refresh', async function () {
-        expect(true).to.be.true
-      })
-      itAll('getTitle', async function () {
-        expect(true).to.be.true
-      })
-      itAll('getWindowHandles/switchToWindow', async function () {
-        expect(true).to.be.true
-      })
-      itAll('switchToFrame/switchToParentFrame', async function () {
-        expect(true).to.be.true
-      })
-      itAll('getWindowRect/setWindowRect', async function () {
-        expect(true).to.be.true
-      })
-      itAll('maximizeWindow/minimizeWindow/fullScreenWindow', async function () {
-        expect(true).to.be.true
-      })
-      itAll('getPageSource', async function () {
-        expect(true).to.be.true
-      })
-      itAll('executeScript/executeAsyncScript', async function () {
-        expect(true).to.be.true
-      })
-      itAll('getAllCookies/getNamedCookie/addCookie/deleteCookie/deleteAllCookies', async function () {
-        expect(true).to.be.true
-      })
-      itAll('dismissAlert/acceptAlert/getAlertText/sendAlertText', async function () {
-        expect(true).to.be.true
-      })
-      itAll('takeScreenshot', async function () {
-        expect(true).to.be.true
-      })
-      itAll('getActiveElement', async function () {
-        expect(true).to.be.true
-      })
-    })
-
-    describe('actions', async function () {
-      itAll('mouse actions', async function () {
-        expect(true).to.be.true
-      })
-      itAll('keyboard actions', async function () {
-        expect(true).to.be.true
-      })
-      itAll('mouse and keyboard actions', async function () {
-        expect(true).to.be.true
-      })
-      itAll('dragging', async function () {
-        expect(true).to.be.true
-      })
-    })
-
-    describe('waitFor', async function () {
-      itAll('wait for an element', async function () {
-        expect(true).to.be.true
-      })
-      itAll('wait for specific window title', async function () {
-        expect(true).to.be.true
-      })
-    })
-
-    describe('elements', async function () {
-      itAll('driver\'s findElement', async function () {
-        expect(true).to.be.true
+      describe(`[${browserName}] actions`, async function () {
+        it('mouse actions', async function () {
+          expect(true).to.be.true
+        })
+        it('keyboard actions', async function () {
+          expect(true).to.be.true
+        })
+        it('mouse and keyboard actions', async function () {
+          expect(true).to.be.true
+        })
+        it('dragging', async function () {
+          expect(true).to.be.true
+        })
       })
 
-      itAll('driver\'s findElementS', async function () {
-        expect(true).to.be.true
+      describe(`[${browserName}] waitFor`, async function () {
+        it('wait for an element', async function () {
+          expect(true).to.be.true
+        })
+        it('wait for specific window title', async function () {
+          expect(true).to.be.true
+        })
       })
 
-      itAll('element\'s isSelected', async function () {
-        expect(true).to.be.true
-      })
-      itAll('element\'s getAttribute', async function () {
-        expect(true).to.be.true
-      })
+      describe(`[${browserName}] elements`, async function () {
+        it('driver\'s findElement', async function () {
+          expect(true).to.be.true
+        })
 
-      itAll('element\'s getProperty', async function () {
-        expect(true).to.be.true
-      })
-      itAll('element\'s getCssValue', async function () {
-        expect(true).to.be.true
-      })
-      itAll('element\'s getText', async function () {
-        expect(true).to.be.true
-      })
-      itAll('element\'s getTagName', async function () {
-        expect(true).to.be.true
-      })
-      itAll('element\'s getRect', async function () {
-        expect(true).to.be.true
-      })
-      itAll('element\'s isEnabled', async function () {
-        expect(true).to.be.true
-      })
-      itAll('element\'s click', async function () {
-        expect(true).to.be.true
-      })
-      itAll('element\'s clear', async function () {
-        expect(true).to.be.true
-      })
-      itAll('element\'s sendKeys', async function () {
-        expect(true).to.be.true
-      })
-      itAll('element\'s takeScreenshot', async function () {
-        expect(true).to.be.true
-      })
-      itAll('element\'s findElement', async function () {
-        expect(true).to.be.true
-      })
-      itAll('element\'s findElements', async function () {
-        expect(true).to.be.true
+        it('driver\'s findElementS', async function () {
+          expect(true).to.be.true
+        })
+
+        it('element\'s isSelected', async function () {
+          expect(true).to.be.true
+        })
+        it('element\'s getAttribute', async function () {
+          expect(true).to.be.true
+        })
+
+        it('element\'s getProperty', async function () {
+          expect(true).to.be.true
+        })
+        it('element\'s getCssValue', async function () {
+          expect(true).to.be.true
+        })
+        it('element\'s getText', async function () {
+          expect(true).to.be.true
+        })
+        it('element\'s getTagName', async function () {
+          expect(true).to.be.true
+        })
+        it('element\'s getRect', async function () {
+          expect(true).to.be.true
+        })
+        it('element\'s isEnabled', async function () {
+          expect(true).to.be.true
+        })
+        it('element\'s click', async function () {
+          expect(true).to.be.true
+        })
+        it('element\'s clear', async function () {
+          expect(true).to.be.true
+        })
+        it('element\'s sendKeys', async function () {
+          expect(true).to.be.true
+        })
+        it('element\'s takeScreenshot', async function () {
+          expect(true).to.be.true
+        })
+        it('element\'s findElement', async function () {
+          expect(true).to.be.true
+        })
+        it('element\'s findElements', async function () {
+          expect(true).to.be.true
+        })
       })
     })
   })
